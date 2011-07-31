801e9b107421a7ed15123d896e943413e9bbf22b
Keiji, Yoshimi <walf443@gmail.com>
2011-07-31 11:19:04 +0900

    ブラウザ経由でもテストが動作するように

--- test/test_goban.js
+++ test/test_goban.js
@@ -1,33 +1,40 @@
-require('../test_helper.js');
-require.paths.push('.');
-var Goban = require('goban.js');
+
+try {
+    require('../test_helper.js');
+    require.paths.push('.');
+    Goban = require('goban.js');
+} catch (e) {
+    // ignore
+}
+
+QUnit.module("Boardに関するテスト", {});
 
 QUnit.test("初期化のテスト", function() {
     var board =  Goban.Board({
         'size': 9,
     });
-    assert.equal(board.data.length, 9*9, 'board size initialized');
+    QUnit.equal(board.data.length, 9*9, 'board size initialized');
 
     for (var i = 0; i < board.data.length; i++ ) {
-        assert.equal(undefined, board.data[i], 'initialized ok');
+        QUnit.equal(undefined, board.data[i], 'initialized ok');
     }
 
-    assert.equal(board.turn, Goban.BLACK, '黒が先番');
+    QUnit.equal(board.turn, Goban.BLACK, '黒が先番');
 });
 
 QUnit.test('hoge', function() {
-    assert.ok(1);
+    QUnit.ok(1);
 });
 
 QUnit.test("隅のアタリ判定", function() {
     var board = new Goban.Board({ 'size': 9 });
     board.move(0, 0);
-    assert.equal(board.turn, Goban.WHITE, 'change turn ok');
+    QUnit.equal(board.turn, Goban.WHITE, 'change turn ok');
     board.move(1, 0);
     board.move(5, 5); // てきとうなところへ打つ
-    assert.equal(board.isDead(1, 2), true, 'アタリであること');
+    QUnit.equal(board.isDead(1, 2), true, 'アタリであること');
     board.move(1, 2);
-    assert.equal(board.turn, Goban.BLACK);
+    QUnit.equal(board.turn, Goban.BLACK);
     // TODO: assert.equal(board.data[0], undefined);
 });
 
