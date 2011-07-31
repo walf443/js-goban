e52695bd780b3786877d0ebcf6b3162a34f4d290
Keiji, Yoshimi <walf443@gmail.com>
2011-07-31 11:29:58 +0900

    test_canvas_view.jsもQUnit対応にする

--- test/test_canvas_view.js
+++ test/test_canvas_view.js
@@ -1,29 +1,36 @@
+
+try {
+    require('../test_helper.js');
+    require.paths.push('.');
+    Goban = require('goban.js');
+} catch (e) {
+}
+
 "use strict";
-// getCoordinateがどのくらいになるかのテスト
+QUnit.module("CanvasViewのテスト", {});
 
-require.paths.push('.');
-var assert = require('assert');
-var Goban = require('goban');
-
-var board = new Goban.Board({
-    'size': 19,
-});
-var view = new Goban.CanvasView({
-    'board': board,
-    'document': {
-        getElementById: function() {
-            return {
-                'height': 300,
-                'width': 300,
-                getContext: function() {
-                },
-            };
+QUnit.test("getCoordinateがどのくらいになるのかのテスト", function() {
+    var board = new Goban.Board({
+        'size': 19,
+    });
+    var view = new Goban.CanvasView({
+        'board': board,
+        'document': {
+            getElementById: function() {
+                return {
+                    'height': 300,
+                    'width': 300,
+                    getContext: function() {
+                    },
+                };
+            },
         },
-    },
+    });
+
+    QUnit.deepEqual([7.894736842105263,7.894736842105263], view.getCoordinate(0, 0), "left top");
+    QUnit.deepEqual([7.894736842105263,292.10526315789474], view.getCoordinate(0, 18), "left bottom");
+    QUnit.deepEqual([292.10526315789474,7.894736842105263], view.getCoordinate(18, 0), "right top");
+    QUnit.deepEqual([292.10526315789474, 292.10526315789474], view.getCoordinate(18, 18), "right bottom");
 });
 
-assert.deepEqual([7.894736842105263,7.894736842105263], view.getCoordinate(0, 0), "left top");
-assert.deepEqual([7.894736842105263,292.10526315789474], view.getCoordinate(0, 18), "left bottom");
-assert.deepEqual([292.10526315789474,7.894736842105263], view.getCoordinate(18, 0), "right top");
-assert.deepEqual([292.10526315789474, 292.10526315789474], view.getCoordinate(18, 18), "right bottom");
-
+QUnit.start();
