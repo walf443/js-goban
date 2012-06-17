c54b069a9edea88349f267e49e4cd1a453387589
Keiji, Yoshimi <walf443@gmail.com>
2012-06-17 15:33:53 +0900

    implement CanvasView.

--- goban.jsx
+++ goban.jsx
@@ -1,4 +1,4 @@
-
+import "js/web.jsx";
 
 class Goban {
     static const NULL: int = 0;
@@ -24,7 +24,7 @@ class Board {
     }
 
     function render(): void {
-        var view = new CLIView(this);
+        var view = new CanvasView(this, 'goban');
         view.render();
     }
 
@@ -102,7 +102,28 @@ class CLIView extends View {
 }
 
 class CanvasView extends View {
+    var backgroundColor = 'rgb(172, 130, 70)';
+    var dom: HTMLCanvasElement;
+    var canvas: CanvasRenderingContext2D;
+
+    function constructor(board: Board) {
+        this.board = board;
+    }
+
+    function constructor(board: Board, id: string) {
+        this.board = board;
+        
+        this.dom = dom.id(id) as HTMLCanvasElement;
+        this.canvas = this.dom.getContext('2d') as CanvasRenderingContext2D;
+    }
+
     override function render(): void {
+        this.drawBoard();
+        for (var i = 0; i < this.board.size; i++ ) {
+            for ( var j = 0; j < this.board.size; j++ ) {
+                this.drawStone(i, j);
+            }
+        }
     }
 
     function drawStone(x:int, y:int): void {
@@ -112,16 +133,95 @@ class CanvasView extends View {
     }
 
     function drawBoard(): void {
+        this.drawBackground();
+        this.canvas.fillStyle = 'rgb(0, 0, 0)';
+        this.canvas.strokeStyle = 'rgb(0, 0, 0)';
+
+        var unit_height = this.dom.height / this.board.size;
+        var unit_width = this.dom.width / this.board.size;
+
+        // vertical line
+        for (var i = 0; i < this.board.size; i++) {
+            this.canvas.beginPath();
+            var start_coordinate = this.getCoordinate(i, 0);
+            this.canvas.moveTo(start_coordinate[0], start_coordinate[1]);
+            var finish_coordinate = this.getCoordinate(i, this.board.size - 1);
+            this.canvas.lineTo(finish_coordinate[0], finish_coordinate[1]);
+            this.canvas.closePath();
+            this.canvas.stroke();
+        }
+
+        // horizontal line
+        for (var j = 0; j < this.board.size; j++) {
+            this.canvas.beginPath();
+            var start_coordinate = this.getCoordinate(0, j);
+            this.canvas.moveTo(start_coordinate[0], start_coordinate[1]);
+            var finish_coordinate = this.getCoordinate(this.board.size - 1, j);
+            this.canvas.lineTo(finish_coordinate[0], finish_coordinate[1]);
+            this.canvas.closePath();
+            this.canvas.stroke();
+        }
+
+        var radius = 2;
+        switch (this.board.size) {
+            case 9:
+                this.point(2, 2, radius);
+                this.point(2, 5 - 1, radius);
+                this.point(2, 9 - 1 - 2, radius);
+                this.point(5 - 1, 9 - 1 - 2, radius);
+                this.point(9 - 1 - 2, 9 - 1 - 2, radius);
+                this.point(9 - 1 - 2, 5 - 1, radius);
+                this.point(9 - 1 - 2, 2, radius);
+                this.point(5 - 1, 2, radius);
+
+                this.point(5 - 1, 5 - 1, radius);
+                break;
+            case 13:
+                // TODO
+                break;
+            case 19:
+                this.point(3, 3, radius);
+                this.point(3, 10 - 1, radius);
+                this.point(3, 19 - 1 - 3, radius);
+                this.point(10 - 1, 19 - 1 - 3, radius);
+                this.point(19 - 1 - 3, 19 - 1 - 3, radius);
+                this.point(19 - 1 - 3, 10 - 1, radius);
+                this.point(19 - 1 - 3, 3, radius);
+                this.point(10 - 1, 3, radius);
+                this.point(10 - 1, 10 - 1, radius);
+                break;
+            default:
+                break;
+        }
+    }
+
+    function point(x:int, y:int, r:number): void {
+        var coordinates =  this.getCoordinate(x, y);
+        this.canvas.beginPath();
+        this.canvas.arc(coordinates[0], coordinates[1], r, 0, Math.PI * 2, false);
+        this.canvas.closePath();
+
+        this.canvas.stroke();
     }
 
     function point(x:int, y:int, r:number, width:int): void {
+        var coordinates =  this.getCoordinate(x, y);
+        this.canvas.beginPath();
+        this.canvas.arc(coordinates[0], coordinates[1], r, 0, Math.PI * 2, false);
+        this.canvas.closePath();
+
+        this.canvas.fill();
     }
 
     function drawBackground(): void {
+        this.canvas.fillStyle = this.backgroundColor;
+        this.canvas.fillRect(0, 0, this.dom.width, this.dom.height);
     }
 
     function getCoordinate(x:int, y:int): number[] {
-        return [1];
+        var unit_height = this.dom.height / this.board.size;
+        var unit_width = this.dom.width / this.board.size;
+        return [unit_width / 2 + unit_width * x, unit_height / 2 + unit_height * y];
     }
 }
 
